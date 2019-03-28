73bd1070a1a0bca82caea2df63a739e753842677
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2019-03-28 18:32:06 +0000

    Fix: updated configurestore.ts

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,21 +1,24 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import { composeWithDevTools } from "redux-devtools-extension";
 import * as reducers from "./reducers/reducers";
+import rootSaga from "./sagas";
 
 const configureStore = ({ initialState = {} }, history) => {
-  const router = routerMiddleware(history);
-  const middleware = [thunk, router];
-
+  const sagaMiddleware = createSagaMiddleware();
+  const middleware = [routerMiddleware(history), sagaMiddleware];
   const reducer = combineReducers({
     ...reducers
   });
+
   const store = createStore(
     connectRouter(history)(reducer),
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
   );
+
+  sagaMiddleware.run(rootSaga, history);
 
   return store;
 };
