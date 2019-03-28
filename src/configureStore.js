b311209926a7f3481bcc95378b32ab5d5c4c6ce0
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2019-03-28 17:42:16 +0000

    deleted comments and redux-thunk package

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,34 +1,21 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
-import createSagaMiddleware from "redux-saga";
-// import thunk from "redux-thunk";
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import * as reducers from "./reducers/reducers";
-import rootSaga from "./sagas";
 
 const configureStore = ({ initialState = {} }, history) => {
-  const sagaMiddleware = createSagaMiddleware();
-  const middleware = [routerMiddleware(history), sagaMiddleware];
-  // const router = routerMiddleware(history);
-  // const middleware = [thunk, router];
+  const router = routerMiddleware(history);
+  const middleware = [thunk, router];
 
   const reducer = combineReducers({
     ...reducers
   });
-
   const store = createStore(
     connectRouter(history)(reducer),
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
   );
-
-  // const store = createStore(
-  //   connectRouter(history)(reducer),
-  //   initialState,
-  //   composeWithDevTools(applyMiddleware(...middleware))
-  // );
-
-  sagaMiddleware.run(rootSaga, history);
 
   return store;
 };
