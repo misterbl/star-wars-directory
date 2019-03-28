99726f8b206a947f07db928f37fab8b683a933b1
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2019-03-28 16:25:54 +0000

    Fix: all tests passing

--- src/actions/actionCreators/__tests__/actions.spec.js
+++ src/actions/actionCreators/__tests__/actions.spec.js
@@ -1,5 +1,5 @@
 import * as actions from "../actions";
-import { actionTypes } from "../actionTypes";
+import actionTypes from "../actionTypes";
 describe("actions", () => {
   describe("fetchingPeopleAndFilms", () => {
     it("should return the fetchingPeopleAndFilms value", () => {
---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
